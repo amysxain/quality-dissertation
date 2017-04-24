8822fd1f926f796058085c70f12269df87d99ab1
Umer Khan <amysxain@live.com>
2017-04-25 00:55:59 +0500

    how it work page added

---
+++ how-it-work.php
@@ -0,0 +1,162 @@
+<?php include 'header.php'; ?>
+    <!--how it work main section -->
+    <section class="about-how-it-work text-center">
+        <div class="container">
+            <div class="lg-head-before icon icon-5"></div>
+            <h2 class="main-head pclr">Essay and Dissertation Writing Service</h2>
+            <p class="regular">For over 10 years we’ve helped thousands of students improve their grades and boost their
+                careers. <br>
+                Your order will be completed to your requirements by an expert writer with a UK Degree.</p>
+            <div class="clearfix"></div>
+            <div class="steps">
+                <div class="steps-image"><img src="images/steps.jpg" alt="" class="img-responsive"></div>
+                <div class="steps-content">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <h3 class="main-head">Enquire</h3>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
+                                has been the industry's standard dummy text ever since the 1500s, when an unknown
+                                printer took a galley of type and scrambled it to make a type specimen book.</p>
+                        </div>
+                        <div class="col-md-3">
+                            <h3 class="main-head">Order</h3>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
+                                has been the industry's standard dummy text ever since the 1500s, when an unknown
+                                printer took a galley of type and scrambled it to make a type specimen book.</p>
+                        </div>
+                        <div class="col-md-3">
+                            <h3 class="main-head">We do the work</h3>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
+                                has been the industry's standard dummy text ever since the 1500s, when an unknown
+                                printer took a galley of type and scrambled it to make a type specimen book.</p>
+                        </div>
+                        <div class="col-md-3">
+                            <h3 class="main-head">Delivery</h3>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
+                                has been the industry's standard dummy text ever since the 1500s, when an unknown
+                                printer took a galley of type and scrambled it to make a type specimen book.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="clearfix"></div>
+
+            <div class="dashboard-about">
+                <div class="lg-head-before icon icon-6"></div>
+                <h2 class="main-head pclr">Essay and Dissertation Writing Service</h2>
+                <p class="regular">For over 10 years we’ve helped thousands of students improve their grades and boost
+                    their
+                    careers. <br>
+                    Your order will be completed to your requirements by an expert writer with a UK Degree.</p>
+                <div class="about-dash-image"><img src="images/admin.jpg" alt="" class="img-responsive"></div>
+            </div>
+        </div>
+    </section>
+    <!--how it work main section end-->
+    <div class="clearfix"></div>
+    <!--Statistical Facts-->
+    <section class="here-to-help counters grad-angled">
+        <div class="man-bg"><img src="images/support.png" alt=""></div>
+        <div class="container">
+            <div class="rox">
+
+                <div class="help-lines text-center">
+                    <h2>Here to help</h2>
+                    <p>Our dedicated team stands by to help you with any questions. Call us on <b>021 1234 1234</b> or
+                        email </p>
+                    <p><b>support@qualitydissertation.com.</b></p>
+                    <div class="location-icon">
+                        <div class="icon icon-7 sm-head-before"></div>
+                        <h3>We're based in the UK</h3>
+                        <p>572 - 574 Romford Road, London, E12 5AF</p>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    </section>
+    <!--Statistical Facts end-->
+    <div class="clearfix"></div>
+    <!--dashboard features -->
+    <section class="dashboard-features">
+        <div class="container">
+            <h3 class="main-head text-center">Once you've submitted an enquiry or placed an order you will have access
+                to <br>
+                <b class="tclr">your own personal dashboard</b>. Using the dashboard you can</h3>
+            <div class="features">
+                <div class="feature">
+                    <div class="icon icon-8 sm-head-before"></div>
+                    <div class="feature-content">
+                        <h4>Own Dashboard</h4>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+                            been the industry's standard dummy text
+                            ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make
+                            a type specim n book. It has survived not only five centuries, but also the leap into
+                            electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
+                            with the release of Letraset sheets containing </p>
+                    </div>
+                </div>
+                <div class="feature">
+                    <div class="icon icon-9 sm-head-before"></div>
+                    <div class="feature-content">
+                        <h4>Send Messages</h4>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+                            been the industry's standard dummy text
+                            ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make
+                            a type specim n book. It has survived not only five centuries, but also the leap into
+                            electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
+                            with the release of Letraset sheets containing </p>
+                    </div>
+                </div>
+                <div class="feature">
+                    <div class="icon icon-10 sm-head-before"></div>
+                    <div class="feature-content">
+                        <h4>Make Orders</h4>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+                            been the industry's standard dummy text
+                            ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make
+                            a type specim n book. It has survived not only five centuries, but also the leap into
+                            electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
+                            with the release of Letraset sheets containing </p>
+                    </div>
+                </div>
+                <div class="feature">
+                    <div class="icon icon-11 sm-head-before"></div>
+                    <div class="feature-content">
+                        <h4>Enquiries</h4>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+                            been the industry's standard dummy text
+                            ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make
+                            a type specim n book. It has survived not only five centuries, but also the leap into
+                            electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
+                            with the release of Letraset sheets containing </p>
+                    </div>
+                </div>
+                <div class="feature">
+                    <div class="icon icon-12 sm-head-before"></div>
+                    <div class="feature-content">
+                        <h4>Admin panel</h4>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+                            been the industry's standard dummy text
+                            ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make
+                            a type specim n book. It has survived not only five centuries, but also the leap into
+                            electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
+                            with the release of Letraset sheets containing </p>
+                    </div>
+                </div>
+                <div class="feature">
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                        the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
+                        of type and scrambled it to make a type specim n book. It has survived not only five centuries,
+                        but also the leap into electronic typesetting, remaining essentially unchanged. It was
+                        popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+                        and more recently with desktop publishing software like Aldus PageMaker including versions of
+                        Lorem Ipsum.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--dashboard features end-->
+<?php include 'footer.php'; ?>
