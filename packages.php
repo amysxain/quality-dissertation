a49455ed3ecd1ea17cb06e8fc1430ca547f420d9
Umer Khan <amysxain@live.com>
2017-04-26 00:11:48 +0500

    package page added

---
+++ packages.php
@@ -0,0 +1,298 @@
+<?php include 'header.php'; ?>
+    <!-- packages page  -->
+
+    <section class="package-page">
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+
+                    <h1 class="main-head tclr">Quality in Best Discounted Prices:</h1>
+                    <h3 class="main-head small">Specialist in Dissertation </h3>
+                    <p>MHR Writer comes with suitable pricing plan which is easily affordable by every student. We never
+                        compromise on quality and in same low price; we are providing professionals written help.
+                        Compare our prices with any other company and you will find us the cheapest service provider in
+                        this industry of academic writing help. We also introduce discounted offers on special occasions
+                        so keep visiting us for getting a huge discount. </p>
+
+                    <p>MHR Writer comes with suitable pricing plan which is easily affordable by every student. We never
+                        compromise on quality and in same low price; we are providing professionals written help.
+                        Compare our prices with any other company and you will find us </p>
+
+                    <a href="" class="btn btn-pclr waves-effect ripple">Get Started Now</a>
+                    <div class="clearfix"></div>
+
+                    <form action="" class="calculator">
+                        <div class="calculate-box m30top m30bottom">
+                            <div class="padding30 pbottom-10">
+                                <div class="icon icon-13 lg-head-before"></div>
+                                <h2 class="main-head pclr text-center">Calculate the price of your order </h2>
+
+                                <p class="regular text-center m20bottom">For over 10 years we’ve helped thousands of
+                                    students improve their grades and boost their
+                                    careers. <br>
+                                    Your order will be completed to your requirements by an expert writer with a UK
+                                    Degree.</p>
+
+
+                                <div class="row">
+                                    <div class="col-md-8">
+                                        <label for="sel">Type of paper needed:</label>
+                                        <select name="" id="sel" class="form-control bordered full-width nm-top">
+                                            <option value="">Value 1</option>
+                                            <option value="">Value 2</option>
+                                            <option value="">Value 3</option>
+                                        </select>
+
+                                    </div>
+                                    <div class="col-md-4">
+                                        <label for="qty">Pages:</label>
+                                        <div class="input-group">
+
+                                      <span class="input-group-btn">
+                                          <button type="button" class="btn btn-tclr btn-number" data-type="minus"
+                                                  data-field="quant[2]">
+                                            <span class="glyphicon glyphicon-minus"></span>
+                                          </button>
+                                      </span>
+
+                                            <input type="text" name="quant[2]"
+                                                   class="form-control  bordered input-number nm-top" value="10"
+                                                   min="1" max="100" id="qty">
+                                            <span class="input-group-btn">
+                                          <button type="button" class="btn btn-pclr btn-number" data-type="plus"
+                                                  data-field="quant[2]">
+                                              <span class="glyphicon glyphicon-plus"></span>
+                                          </button>
+                                        </span>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div class="clearfix"></div>
+                            <div class="grad-bg">
+                                <h3>Admission Essay/Personal Statement Prices</h3>
+                            </div>
+
+                            <div class="packages-tabs">
+
+
+                                <div>
+
+
+                                    <!-- Nav tabs -->
+                                    <ul class="nav nav-tabs" role="tablist">
+                                        <li role="presentation" class="active"><a href="#package-1"
+                                                                                  aria-controls="package-1" role="tab"
+                                                                                  data-toggle="tab">High School</a></li>
+                                        <li role="presentation"><a href="#package-2" aria-controls="package-2"
+                                                                   role="tab" data-toggle="tab">Undergrad. (yrs 1-2)</a>
+                                        </li>
+                                        <li role="presentation"><a href="#package-3" aria-controls="package-3"
+                                                                   role="tab" data-toggle="tab">Undergrad. (yrs 3-4)</a>
+                                        </li>
+                                        <li role="presentation"><a href="#package-4" aria-controls="package-4"
+                                                                   role="tab" data-toggle="tab">Master's</a></li>
+                                        <li role="presentation"><a href="#package-4" aria-controls="package-5"
+                                                                   role="tab" data-toggle="tab">Doctoral</a></li>
+                                    </ul>
+
+                                    <!-- Tab panes -->
+                                    <div class="tab-content">
+                                        <div role="tabpanel" class="tab-pane fade in active" id="package-1">
+
+                                            <div class="col-xs-2 rates">
+                                                <label for="planselect1">
+                                                    <input type="radio" name="plan" class="sr-only" id="planselect1"
+                                                           checked>
+                                                    <div class="checkbox-box">
+                                                        <span class="radio-custom">
+                                                            <span></span>
+                                                        </span>
+                                                        <div class="price">$16</div>
+                                                        <p>page</p>
+                                                        <div class="day">14d</div>
+                                                        <p class="deadline">deadline</p>
+                                                        <div class="date">09 May 2017</div>
+                                                    </div>
+                                                </label>
+                                            </div>
+
+                                            <div class="col-xs-2 rates">
+                                                <label for="planselect2">
+                                                    <input type="radio" name="plan" class="sr-only" id="planselect2">
+                                                    <div class="checkbox-box">
+                                                        <span class="radio-custom">
+                                                            <span></span>
+                                                        </span>
+                                                        <div class="price">$16</div>
+                                                        <p>page</p>
+                                                        <div class="day">14d</div>
+                                                        <p class="deadline">deadline</p>
+                                                        <div class="date">09 May 2017</div>
+                                                    </div>
+                                                </label>
+                                            </div>
+
+                                            <div class="col-xs-2 rates">
+                                                <label for="planselect3">
+                                                    <input type="radio" name="plan" class="sr-only" id="planselect3">
+                                                    <div class="checkbox-box">
+                                                        <span class="radio-custom">
+                                                            <span></span>
+                                                        </span>
+                                                        <div class="price">$16</div>
+                                                        <p>page</p>
+                                                        <div class="day">14d</div>
+                                                        <p class="deadline">deadline</p>
+                                                        <div class="date">09 May 2017</div>
+                                                    </div>
+                                                </label>
+                                            </div>
+
+                                            <div class="col-xs-2 rates">
+                                                <label for="planselect4">
+                                                    <input type="radio" name="plan" class="sr-only" id="planselect4">
+                                                    <div class="checkbox-box">
+                                                        <span class="radio-custom">
+                                                            <span></span>
+                                                        </span>
+                                                        <div class="price">$16</div>
+                                                        <p>page</p>
+                                                        <div class="day">14d</div>
+                                                        <p class="deadline">deadline</p>
+                                                        <div class="date">09 May 2017</div>
+                                                    </div>
+                                                </label>
+                                            </div>
+
+                                            <div class="col-xs-2 rates">
+                                                <label for="planselect5">
+                                                    <input type="radio" name="plan" class="sr-only" id="planselect5">
+                                                    <div class="checkbox-box">
+                                                        <span class="radio-custom">
+                                                            <span></span>
+                                                        </span>
+                                                        <div class="price">$16</div>
+                                                        <p>page</p>
+                                                        <div class="day">14d</div>
+                                                        <p class="deadline">deadline</p>
+                                                        <div class="date">09 May 2017</div>
+                                                    </div>
+                                                </label>
+                                            </div>
+
+                                            <div class="col-xs-2 rates">
+                                                <label for="planselect6">
+                                                    <input type="radio" name="plan" class="sr-only" id="planselect6">
+                                                    <div class="checkbox-box">
+                                                        <span class="radio-custom">
+                                                            <span></span>
+                                                        </span>
+                                                        <div class="price">$16</div>
+                                                        <p>page</p>
+                                                        <div class="day">14d</div>
+                                                        <p class="deadline">deadline</p>
+                                                        <div class="date">09 May 2017</div>
+                                                    </div>
+                                                </label>
+                                            </div>
+
+                                            <div class="clearfix"></div>
+                                        </div>
+                                        <div role="tabpanel" class="tab-pane fade" id="package-2">...</div>
+                                        <div role="tabpanel" class="tab-pane fade" id="package-3">...</div>
+                                        <div role="tabpanel" class="tab-pane fade" id="package-4">...</div>
+                                        <div role="tabpanel" class="tab-pane fade" id="package-5">...</div>
+                                    </div>
+
+                                </div>
+
+
+                            </div>
+
+                            <div class="clearfix"></div>
+
+
+                            <div class="package-footer">
+
+                                <div class="col-md-8">
+                                    <p>Place an order within a couple of minutes.
+                                        Get guaranteed assistance and 100% confidentiality. </p>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="total-price"> Total price: <b class="tclr">$0.00</b></div>
+                                    <button class="btn btn-tclr waves-effect ripple">Continue to Order</button>
+                                </div>
+                            </div>
+                            <div class="clearix"></div>
+
+
+                        </div>
+
+                    </form>
+
+
+                    <div class="clearfix"></div>
+                    <p>MHR Writer comes with suitable pricing plan which is easily affordable by every student. We never
+                        compromise on quality and in same low price; we are providing professionals written help.
+                        Compare our prices with any other company and you will find us the cheapest service provider in
+                        this industry of academic writing help. We also introduce discounted offers on special occasions
+                        so keep visiting us for getting a huge discount. </p>
+
+                    <p>MHR Writer comes with suitable pricing plan which is easily affordable by every student. We never
+                        compromise on quality and in same low price; we are providing professionals written help.
+                        Compare our prices with any other company and you will find us </p>
+
+                    <p>MHR Writer comes with suitable pricing plan which is easily affordable by every student. We never
+                        compromise on quality and in same low price; we are providing professionals written help.
+                        Compare our prices with any other company and you will find us the cheapest service provider in
+                        this industry of academic writing help. We also introduce discounted offers on special occasions
+                        so keep visiting us for getting a huge discount. MHR Writer comes with suitable pricing plan
+                        which is easily affordable by every student. We never
+                        compromise on quality and in same low price; we are providing professionals written help.
+                        Compare our prices with any other company and you will find us the cheapest service provider in
+                        this industry of academic writing help. We also introduce discounted offers on special occasions
+                        so keep visiting us for getting a huge discount. </p>
+                </div>
+                <aside class="col-md-4">
+
+                    <div class="box-shadowed">
+                        <h3 class="headed-bg main-head small tbg">Benefits You Get</h3>
+                        <div class="icon icon-14 lg-head-before"></div>
+                        <p>MHR Writer comes with suitable pricing plan which is easily affordable by every student. We
+                            never compromise on quality and in same low price; we are providing professionals written
+                            help. Compare our prices with any other company and</p>
+                    </div>
+
+                    <div class="call-box">
+                        <h4 class="call-head">24/7 Call support</h4>
+                        <div class="call-icon"><img src="images/call.png" alt=""></div>
+                        <div class="call-number">+44 800 048 8966</div>
+                    </div>
+
+                    <div class="request-call">
+                        <h3 class="main-head">Request Call Back</h3>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+                            been the industry's standard dummy text </p>
+                        <form action="">
+                            <p><input type="text" class="form-control" placeholder="How can we help?"></p>
+                            <p><input type="text" class="form-control" placeholder="Your name"></p>
+                            <p><input type="text" class="form-control" placeholder="Your email"></p>
+                            <p>
+                                <button class="btn btn-pclr waves-effect ripple">Submit</button>
+                            </p>
+                        </form>
+                    </div>
+
+
+                </aside>
+            </div>
+        </div>
+
+
+    </section>
+
+    <!-- packages page end -->
+<?php include 'footer.php'; ?>
